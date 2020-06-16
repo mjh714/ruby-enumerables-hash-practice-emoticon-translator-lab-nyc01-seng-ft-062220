f3377482b768332cabe3c19b6a6669a9c2a8e830
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-16 19:04:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return  my_hash[key][:japanese]
     end
   end
-  return "Sorry, that moticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
